8dda422fea4d268d6a5845ad9769b9d3329bbde5
DmityFilchev <filchev_dmitry@mail.ru>
2022-10-21 11:49:51 +0300

    '21_10'

--- Task24_2/Task24_2.cpp
+++ Task24_2/Task24_2.cpp
@@ -42,13 +42,36 @@ int main()
 	int temp_month = t.tm_mon;
 	int temp_day = t.tm_mday;
 	find_birthday = birthday.lower_bound(temp_month);
+	bool not_birthday= true;
 	while (find_birthday != birthday.end())
 	{
-		auto beg = find_birthday->second.lower_bound(temp_day);
-		if (beg->second.empty())
-			continue;
-		for (const auto& birthday : find_birthday->second.lower_bound(temp_day)->second)
-			std::cout << birthday << std::endl;
+		if ((find_birthday->first)-1 > t.tm_mon)
+		{
+			std::cout <<"Month " << find_birthday->first << " " <<"Day "<< find_birthday->second.lower_bound(0)->first << std::endl;
+			for (const auto& names : find_birthday->second.lower_bound(0)->second)
+				std::cout << names << std::endl;
+			not_birthday = false;
+		}
+		else if ((find_birthday->first)-1 == t.tm_mon)
+		{
+			auto beg = find_birthday->second.lower_bound(temp_day);
+			if (beg != find_birthday->second.end())
+			{
+				
+				std::cout << "Month " << find_birthday->first << " " << "Day " << find_birthday->second.lower_bound(0)->first << std::endl;
+				if (beg->first == temp_day)
+					std::cout << "Today is the birthday !!!!!\n";
+				for (const auto& names : find_birthday->second.lower_bound(temp_day)->second)
+					std::cout << names << std::endl;
+				not_birthday = false;
+				break;
+			}
+
+		}
 		find_birthday++;
+	}
+	if (not_birthday)
+	{
+		std::cout << "There are no birthdays until the end of this year\n";
 	}
 }
